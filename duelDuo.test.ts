28b1ebc31ec8fdf59731e2c71de5e92ce10f0f85
Erick Estrada <erickdallas30@gmail.com>
2022-12-18 02:58:48 -0600

    automated test added

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -6,7 +6,7 @@ require('chromedriver')
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
 beforeEach(async () => {
-    driver.get('http://localhost:3000/')
+    driver.get('http://localhost:4000/')
 })
 
 afterAll(async () => {
@@ -17,4 +17,23 @@ test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
+})
+
+
+test('Check that the Draw button works when clicking on it', async () => {
+    await driver.findElement(By.id('draw')).click()
+    await driver.sleep(2000)
+    const randomBots = await driver.findElement(By.id('choices'))
+    const displayedRandomBots = randomBots.isDisplayed()
+    expect(displayedRandomBots).toBeTruthy()       
+})
+
+//this test is intended to check the bug on the see all bots button, in this case the see alll bots buttons
+// is not working so expect this test to fail, if I would fix the see all bots button bugs It will return 3 passed results
+test('Check that the Add to Duo button works when clicking on it', async () => {
+    await driver.findElement(By.id('see-all')).click()
+    await driver.sleep(2000)    
+    const allbots = await driver.findElement(By.className('bot-card'))
+    const displayedBots = allbots.isDisplayed()
+    expect(displayedBots).toBeTruthy()
 })
--- functions.test.ts
+++ functions.test.ts
@@ -2,4 +2,12 @@ const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
     // CODE HERE
+    let Arr = shuffleArray([1,2,3,4,5])
+    test('Check if shuffleArray returns a value type of array', () => {
+        expect(Array.isArray(Arr)).toBe(true)
+    })
+    
+    test('Check if shuffleArray returns the same array length as the argument sent in', () => {
+        expect(Arr).toHaveLength(5)
+    })
 })
